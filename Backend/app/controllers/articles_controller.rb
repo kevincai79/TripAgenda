c2a5c1e18db34e2ed35879f628d67348b29f1159
Kevin Cai <kevincai1979@gmail.com>
2017-12-17 23:59:30 -0800

    add edit page

--- Backend/app/controllers/articles_controller.rb
+++ Backend/app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
